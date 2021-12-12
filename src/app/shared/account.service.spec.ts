ca94690a8815ed09fdaf20e129da272dbbf08188
StaraxGG <nicolasklein1998@gmail.com>
2021-12-12 14:09:06 +0100

    Fixed failing angular example tests

--- src/app/shared/account.service.spec.ts
+++ src/app/shared/account.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 describe('AccountService', () => {
   let service: AccountService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [
+        RouterModule,
+        AppRoutingModule,
+        HttpClientModule,
+      ],});
     service = TestBed.inject(AccountService);
   });
 
