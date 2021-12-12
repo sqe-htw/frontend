e18f6cd210deee6d8a3344e55f53dc8cc3ddca80
StaraxGG <nicolasklein1998@gmail.com>
2021-12-12 14:01:12 +0100

    Fixed register component test imports

--- src/app/modules/auth/register/register.component.spec.ts
+++ src/app/modules/auth/register/register.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "../../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {AccountService} from "../../../shared/account.service";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,6 +13,14 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AppRoutingModule,
+        HttpClientModule,
+      ],
+      providers: [ AccountService ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
