c45bc56deb39a9eb5258632d306cf7dd014a74c6
StaraxGG <nicolasklein1998@gmail.com>
2021-12-12 12:17:24 +0100

    Fixed login tests not running due to missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { CardComponent } from './modules/playground/card/card.component';
 import { HeaderComponent } from './core/header/header.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    HeaderComponent
+    HeaderComponent,
     
   ],
   imports: [
@@ -23,7 +25,9 @@ import { HeaderComponent } from './core/header/header.component';
     AuthModule,
     HttpClientModule,
     CoreModule,
-    
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/auth/login/login.component.spec.ts
+++ src/app/modules/auth/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "../../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +12,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+          FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AppRoutingModule,
+        HttpClientModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
