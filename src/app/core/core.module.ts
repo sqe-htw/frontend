e6cd64d80275f535ab48804c104569b147c053d7
StaraxGG <nicolasklein1998@gmail.com>
2021-12-20 13:37:35 +0100

    Added main menue

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MenueComponent } from './main_menue/menue/menue.component';
 
 
 
 @NgModule({
-  declarations: [ HomeComponent],
+  declarations: [ HomeComponent, MenueComponent],
   imports: [
     CommonModule,
     RouterModule
---
+++ src/app/core/main_menue/menue/menue.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from "../../../shared/account.service";
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'app-menue',
+    templateUrl: './menue.component.html',
+    styleUrls: ['./menue.component.sass']
+})
+export class MenueComponent implements OnInit {
+
+    userName!: String;
+
+    constructor(private accountService: AccountService,
+                private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.userName = this.accountService.userValue.user.username;
+    }
+
+    logout() {
+      this.accountService.logout();
+      this.router.navigateByUrl('');
+    }
+
+}
