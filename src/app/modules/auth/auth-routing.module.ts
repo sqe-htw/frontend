8d0609c6c84375ee0ffd3e9d6868e11da31dae14
StolzAlexander <alexander.stolz97@gmail.com>
2021-12-30 17:54:07 +0100

    addded 'export' for routes

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -6,7 +6,7 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 import {MenuComponent} from "../../core/main_menu/menu/menu.component";
 import {AccountService} from "../../shared/account.service";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
