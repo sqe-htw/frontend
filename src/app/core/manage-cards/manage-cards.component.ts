c4aef9ca7e325385fa36cbfe54eb46b28313ee49
StaraxGG <nicolasklein1998@gmail.com>
2022-01-09 12:56:57 +0100

    Cards can now be displayed

--- src/app/core/manage-cards/manage-cards.component.ts
+++ src/app/core/manage-cards/manage-cards.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AccountService} from "../../shared/account.service";
 import {GameService} from "../../shared/game.service";
 import {Router} from "@angular/router";
+import {first} from "rxjs/operators";
+import {Card} from "../../models/card";
 
 @Component({
   selector: 'app-manage-cards',
@@ -10,11 +12,31 @@ import {Router} from "@angular/router";
 })
 export class ManageCardsComponent implements OnInit {
 
+  userToken!: string;
+  userId!: number;
+  public cards: Card[] = [];
+
   constructor(private accountService: AccountService,
               private gameService: GameService,
               private router: Router) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    console.log(this.accountService.currentUser().access_token)
+    this.userToken = this.accountService.currentUser().access_token;
+    this.userId = this.accountService.currentUser().user.id;
+
+    await this.getAllCards(this.userId, this.userToken);
+  }
+
+  getAllCards(userId: number, userToken: string) {
+    this.gameService.getAllCards(userId, userToken).pipe(first())
+        .subscribe({
+          next: (cards: Card[]) => {
+            console.log(cards);
+            this.cards = cards;
+          },
+        });
+    console.log(this.cards)
   }
 
 }
