92608eecc2a330602557941bb9243bae3e6d0d45
StaraxGG <nicolasklein1998@gmail.com>
2022-01-09 13:15:48 +0100

    Card input form structure

--- src/app/core/manage-cards/manage-cards.component.ts
+++ src/app/core/manage-cards/manage-cards.component.ts
@@ -4,6 +4,8 @@ import {GameService} from "../../shared/game.service";
 import {Router} from "@angular/router";
 import {first} from "rxjs/operators";
 import {Card} from "../../models/card";
+import {UserAuth} from "../../models/user";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-manage-cards',
@@ -16,11 +18,22 @@ export class ManageCardsComponent implements OnInit {
   userId!: number;
   public cards: Card[] = [];
 
+  public createError: boolean = false;
+  public loading: boolean = false;
+  submitted = false;
+  valid = false;
+
+  form!: FormGroup;
+
   constructor(private accountService: AccountService,
               private gameService: GameService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   async ngOnInit(): Promise<void> {
+    this.form = this.formBuilder.group({
+      cardText: ['', [Validators.required, Validators.minLength(1)]]
+    });
     console.log(this.accountService.currentUser().access_token)
     this.userToken = this.accountService.currentUser().access_token;
     this.userId = this.accountService.currentUser().user.id;
@@ -37,6 +50,26 @@ export class ManageCardsComponent implements OnInit {
           },
         });
     console.log(this.cards)
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit(): void {
+    console.log("__debug: onSubmit create text component");
+
+    // this.submitted = true;
+
+    if (this.form.invalid) {
+      this.valid = false;
+      return;
+    }
+    this.valid = true;
+
+    console.log("Value:" +this.form.value.cardText);
+    // this.loading = true;
+
+    this.form.reset();
+
   }
 
 }
