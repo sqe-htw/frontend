caed100848ce8b850172546212d5970d2183eeef
StolzAlexander <alexander.stolz97@gmail.com>
2022-01-20 14:30:33 +0100

    check if user is logged in. If true, display menu with settings for logout and reset-password. Added user icon and brand icon

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,10 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Router, RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
+import {of} from "rxjs";
+import {User, UserRegister} from '../../models/user';
+import {AccountService} from "../../shared/account.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {Location} from "@angular/common";
+import { routes } from "../../app-routing.module";
 import { HeaderComponent } from './header.component';
+
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let de: DebugElement;
+
+  let service: AccountService;
+
+  let spy: jasmine.Spy;
+
+  let router: Router;
+
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,13 +34,44 @@ describe('HeaderComponent', () => {
     .compileComponents();
   });
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        HttpClientModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+      providers: [ AccountService ],
+      declarations: [ HeaderComponent ]
+    })
+        .compileComponents();
+  });
+
   beforeEach(() => {
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    service = de.injector.get(AccountService);
+
     fixture.detectChanges();
+    router.initialNavigation();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate while loggedIn to main-menu', fakeAsync(() => {
+
+    router.navigate(['/main-menu'])
+    tick();
+    expect(location.path()).toBe('/main-menu')
+  }));
+
 });
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AccountService} from "../../shared/account.service";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  userName!: String;
+  loggedIn!: boolean;
+  constructor(private accountService: AccountService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.accountService.currentLoggedIn.subscribe(currentLoggedIn => this.loggedIn = currentLoggedIn);
+    this.userName = this.accountService.currentUser().user.username;
+  }
+
+  /**
+   * Falls User eingeloggt ist, navigiere bei click aufs Logo zum Spielhauptmenü.
+   * Andernfalls navigiere zum router-outlet
+   */
+  navigate(){
+    console.log(this.loggedIn)
+    if(this.loggedIn == false){
+      this.router.navigateByUrl('');
+    }else{
+      this.router.navigateByUrl('main-menu');
+    }
+  }
+
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('');
+  }
+
+  resetPassword() {
+    this.router.navigateByUrl('reset-password');
   }
 
 }
