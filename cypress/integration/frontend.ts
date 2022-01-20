0fe41cde45b78693d0afd76cff4da700f58a42d0
StaraxGG <nicolasklein1998@gmail.com>
2022-01-20 15:56:17 +0100

    Added e2e test journey

--- cypress/integration/frontend.ts
+++ cypress/integration/frontend.ts
@@ -1,9 +1,48 @@
 describe('Frontend', () => {
     beforeEach(() => {
+
+        /// Backend starten
+
+        /// Spawn, in Hintergrund Thread backend startenng test --no-watch --code-coverage
+
         cy.visit('/');
     });
 
     it('has the correct title', () => {
         cy.title().should('equal', 'Drinking Game');
+    });
+
+    it('user login and register journey', () => {
+        const uuid = () => Cypress._.random(0, 1e10)
+        const unique_username = uuid();
+
+        /// Register and Login Buttons do exist and have the correct content.
+        cy.get('[data-testid="login-button"]').should($p => {
+            expect($p.first()).to.contain('Login');
+        });
+        cy.get('[data-testid="register-button"]').should($p => {
+            expect($p.first()).to.contain('Registrieren');
+        });
+
+        /// Register workflow is successful for new user
+        cy.get('[data-testid="register-button"]').click();
+        cy.url().should('include', '/register');
+        cy.get('[data-testid="username-input"]').type(unique_username.toString());
+        cy.get('[data-testid="password-input"]').type(unique_username.toString());
+
+        cy.get('[data-testid="submit-form-button"]').click();
+
+        /// Login workflow is successful
+        cy.url().should('include', '/login');
+
+        cy.get('[data-testid="username-input"]').type(unique_username.toString());
+        cy.get('[data-testid="password-input"]').type(unique_username.toString());
+
+        cy.get('[data-testid="submit-login-button"]').click();
+        cy.url().should('include', '/main-menu');
+
+        /// Logout
+        cy.get('[data-testid="logout-button"]').click();
+        cy.url().should('eq', Cypress.config().baseUrl+'/');
     });
 });
